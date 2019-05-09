3154e94be981d6631927f99f6a480438deb8ab62
sara-nademi <50390099+sara-nademi@users.noreply.github.com>
2019-05-08 21:54:43 -0400

    Add files via upload

---
+++ SaraNademiMovieStore/Controllers/RentalsController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SaraNademiMovieStore.Controllers
+{
+    public class RentalsController : Controller
+    {
+        public ActionResult New()
+        {
+            return View();
+        }
+    }
+}
